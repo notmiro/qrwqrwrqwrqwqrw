44bfa5f14c582a3e1accb63684cab6d9d724df37
Vuuuult <43483379+Vuuuult@users.noreply.github.com>
2018-10-12 16:07:10 -0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('message', msg => {
+
+    if (msg.content == ',join') {
+        if (msg.member.voiceChannel) {
+
+     if (msg.member.voiceChannel.joinable) {
+         msg.member.voiceChannel.join().then(msg.react('✅'));
+     }
+    }
+}
+})
+
+client.login(process.env.BOT_TOKEN);
