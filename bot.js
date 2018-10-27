d6869907b9ad21064c61a01f868cb41a527e36ec
Vuuuult <43483379+Vuuuult@users.noreply.github.com>
2018-10-27 03:37:01 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,9 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on('message', msg => {
-
-    if (msg.content == ',join') {
-        if (msg.member.voiceChannel) {
-
-     if (msg.member.voiceChannel.joinable) {
-         msg.member.voiceChannel.join().then(msg.react('✅'));
-     }
-    }
-}
-})
+client.on('ready', () => {
+var x = client.channels.get("آيدي الروم");
+if (x) x.join();
+});
 
 client.login(process.env.BOT_TOKEN);
